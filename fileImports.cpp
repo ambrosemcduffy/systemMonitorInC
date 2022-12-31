5a2f8ca13484ff905b11da1006ec970d7d81d4d3
Ambrose McDuffy <ambrosemcduffy@gmail.com>
2022-12-30 18:58:02 -0800

    updating

--- fileImports.cpp
+++ fileImports.cpp
@@ -1,83 +1,119 @@
-#include <vector>
-#include <iostream>
 #include <fstream>
-#include <sstream>
+#include <iostream>
 #include <regex>
-#include "helpFile.h"
-//void readMemUtilization(){}
+#include <sstream>
+#include <vector>
 
+#include "helperFile.h"
 
-using namespace std;
-string getOsInfo(){
-    string path = "/etc/os-release";
-    string line;
-    string key;
-    string value;
-    ifstream file;
+std::string getOsInfo() {
+  std::string path = "/etc/os-release";
+  std::string line;
+  std::string key;
+  std::string value;
+  std::ifstream file;
 
-    file.open(path);
-    while (getline(file, line)){
-        replace(line.begin(), line.end(), ' ', '_');
-        replace(line.begin(), line.end(), '=', '_');
-        replace(line.begin(), line.end(), '"', ' ');
-        istringstream mystream(line);
-        while(mystream >> key >> value){
-            if (key == "PRETTY_NAME_"){
-                return value;
-            }
-        }
+  file.open(path);
+  while (getline(file, line)) {
+    replace(line.begin(), line.end(), ' ', '_');
+    replace(line.begin(), line.end(), '=', '_');
+    replace(line.begin(), line.end(), '"', ' ');
+    std::istringstream mystream(line);
+    while (mystream >> key >> value) {
+      if (key == "PRETTY_NAME_") {
+        return value;
+      }
     }
-    return value;
+  }
+  return value;
+}
+
+int getMemUtilization() {
+    /*
+    This function returns the cpu utilization
+    */
+  std::string path = "/proc/meminfo";
+  std::ifstream file;
+  std::string line;
+  std::string key;
+
+  int value;
+  int memFree{0};
+  int memTotal{0};
+  file.open(path);
+
+  while (getline(file, line)) {
+    std::stringstream mystream(line);
+    mystream >> key >> value;
+    if (key == "MemTotal:") {
+      memTotal += value;
+    }
+    if (key == "MemFree:") {
+      memFree += value;
+    }
+  }
+  return memTotal - memFree;
+}
+
+std::string getKernelVersion() {
+  /*
+  This function get the kernel verison
+  of the linux OS
+  */
+  std::string line;
+  std::string kernelVersion;
+
+  std::ifstream file;
+  file.open("/proc/version");
+  if (file) {
+    while (getline(file, line)) {
+      std::string kernelVersion = split(split(line, ' ')[2], '-')[0];
+      return kernelVersion;
+    }
+  }
+  return kernelVersion;
 }
 
 
-int getMemUtilization(){
-    string path = "/proc/meminfo";
-    ifstream file;
-    string line;
-    string key;
-    
-    int value; 
-    int memFree{0};
-    int memTotal{0};
-    file.open(path);
-    
-    while (getline(file, line)){
-        stringstream mystream(line);
-        mystream >> key >> value;
-        if (key == "MemTotal:"){
-            memTotal += value;
-        }
-        if (key == "MemFree:"){
-            memFree += value;
-        }
+std::map<std::string, std::string> getNumProcesses() {
+  std::ifstream file;
+  std::string line;
+  std::map<std::string, std::string> processDict;
+  file.open("/proc/stat");
+
+  while (getline(file, line)) {
+    if (line[0] != 'p') {
+      continue;
     }
-    return memTotal - memFree;
+    std::vector<std::string> _keyValueList = split(line, ' ');
+    if (_keyValueList[0] == "processes") {
+      processDict["processes"] = _keyValueList[1];
+    }
+
+    if (_keyValueList[0] == "procs_running") {
+      processDict["procs_running"] = _keyValueList[1];
+    }
+  }
+  return processDict;
 }
 
-
-string getKernelVersion(){
-    string line;
-    string version;
-    string path = "/proc/version";
-    ifstream file;
-    file.open(path);
-    if (file){
-        while (getline(file, line)){
-            vector<string> _arr1 = split(line, ' ');
-            printArray(_arr1);
-            //vector<string> elementArray = split(_arr1[2], '-');
-            //version = elementArray[0];
-            //return slice(std::move(version), std::move(0), std::move(0));
-        }
-    }
-    return version;
-
+std::string getUpTime() {
+  std::ifstream file;
+  std::string line;
+  file.open("/proc/uptime");
+  while (getline(file, line)) {
+    return line;
+  }
+  return line;
 }
 
-
-int main(){
-    std::cout << getKernelVersion() << std::endl;;
-    //std::cout << getMemUtilization() << std::endl;
-    //std::cout << getOsInfo() << std::endl;
+int main() {
+  std::cout << "Current Uptime: " << getUpTime() << std::endl;
+  std::cout << "Number of Processes: " << getNumProcesses()["processes"]
+            << std::endl;
+  std::cout << "Total Number of Processes: "
+            << getNumProcesses()["procs_running"] << std::endl;
+  std::cout << "Kernel Version: " << getKernelVersion() << std::endl;
+  std::cout << "Memory Usage: " << getMemUtilization() << std::endl;
+  std::cout << "Operating system in use: " << getOsInfo() << std::endl;
 }
